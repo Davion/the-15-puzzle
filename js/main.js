35bf93bf87da9b1df010db2a9718d77595da7326
Klajdi Hoxha <klainti95@gmail.com>
2021-03-12 16:14:36 +0100

    movement impl

--- js/main.js
+++ js/main.js
@@ -1,7 +1,36 @@
 const board = document.querySelector(".board");
 
+const gameTiles = Object.values(board.childNodes).filter(tile => tile.nodeName !== "#text");
+console.log(gameTiles);
+
+
 board.addEventListener("click", moveTile);
 
 function moveTile(e){
-    console.log(e.target.classList.contains("tile"));
+    // console.log(e.target.classList.contains("tile"));
+    if(e.target.classList.contains("tile")){
+        let targetIdx = gameTiles.indexOf(e.target)
+        let moved = false;
+        if(targetIdx % 4 !== 3 && gameTiles[targetIdx + 1].classList.contains("empty")){
+            [gameTiles[targetIdx], gameTiles[targetIdx + 1]] = [gameTiles[targetIdx + 1], gameTiles[targetIdx]];
+            moved = true;
+        }else if(targetIdx % 4 !== 0 && gameTiles[targetIdx - 1].classList.contains("empty")){
+            [gameTiles[targetIdx], gameTiles[targetIdx - 1]] = [gameTiles[targetIdx - 1], gameTiles[targetIdx]];
+            moved = true;
+        }else if(gameTiles[targetIdx - 4] && gameTiles[targetIdx - 4].classList.contains("empty")){
+            [gameTiles[targetIdx], gameTiles[targetIdx - 4]] = [gameTiles[targetIdx - 4], gameTiles[targetIdx]];
+            moved = true;
+        }else if(gameTiles[targetIdx + 4] && gameTiles[targetIdx + 4].classList.contains("empty")){
+            [gameTiles[targetIdx], gameTiles[targetIdx + 4]] = [gameTiles[targetIdx + 4], gameTiles[targetIdx]];
+            moved = true;
+        }
+
+        if(moved){
+            console.log("moved");
+            gameTiles.forEach(tile => {
+                tile.parentNode.removeChild(tile);
+                board.appendChild(tile);
+            });
+        }
+    }
 }
