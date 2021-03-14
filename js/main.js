6a473a5304e57cfde47d552a5e0eed8432b208d6
Klajdi Hoxha <klainti95@gmail.com>
2021-03-14 23:37:43 +0100

    register service worker

--- js/main.js
+++ js/main.js
@@ -63,3 +63,13 @@ function moveTile(e){
         }
     }
 }
+
+// Register Service Worker
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/todo-app-js-pwa/serviceWorker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err));
+    });
+  }
